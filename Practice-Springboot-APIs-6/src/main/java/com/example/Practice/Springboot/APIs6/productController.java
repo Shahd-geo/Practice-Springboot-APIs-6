4be4511ef12ce92ed3a6a7b428127b0eae0da61e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 11:28:53 +0400

    create hashmap to store data

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/productController.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/productController.java
@@ -2,6 +2,9 @@ package com.example.Practice.Springboot.APIs6;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class productController {
+    HashMap<Integer, Product> products = new HashMap<>();
 }
