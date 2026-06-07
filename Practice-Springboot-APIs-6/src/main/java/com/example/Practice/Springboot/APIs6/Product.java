827ed12870e9dee42951d5e979d70761c972b3a0
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 11:24:25 +0400

    CREATE GETTER AND SETTER USING LOMBOK NOTAION

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/Product.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/Product.java
@@ -1,4 +1,14 @@
 package com.example.Practice.Springboot.APIs6;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Product {
+    private  int id;
+    private  String name;
+    private  int stockQuantity;
 }
