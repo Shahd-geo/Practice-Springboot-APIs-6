1e884b90519fe8f6ec429f34d7080163310aa060
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 10:56:55 +0400

    Using Java Basics for UPDATE Logic – Email Update Example

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/UpdateEmailDemo.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/UpdateEmailDemo.java
@@ -1,4 +1,35 @@
 package com.example.Practice.Springboot.APIs6;
 
 public class UpdateEmailDemo {
+    public static void main(String[] args) {
+        updateEmail();
+    }
+
+    public static void updateEmail() {
+        String oldEmail = "Ahmed@Example.com";
+        String newEmail = "ahmed@example.com";
+        boolean isUpdated;
+
+        System.out.println("Original Email: " + oldEmail);
+
+        if (!oldEmail.equalsIgnoreCase(newEmail)) {
+
+            oldEmail = newEmail;
+
+            isUpdated = true;
+
+            System.out.println("Email updated successfully.");
+
+        } else {
+
+            isUpdated = false;
+
+            System.out.println("No update required. Email address is already current."
+            );
+        }
+
+        System.out.println("Updated Email: " + oldEmail);
+
+        System.out.println("Update Status: " + isUpdated);
+    }
 }
