3bcb126ed1bf5e2194f259fc553af3da9a5337a5
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 11:33:58 +0400

    CREATE updateStock FUNCTION

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/productController.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/productController.java
@@ -1,5 +1,7 @@
 package com.example.Practice.Springboot.APIs6;
 
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,4 +13,7 @@ public class productController {
     public productController() {
         products.put(1, new Product(1, "Laptop", 10));
     }
-}
+    @PutMapping("/updateStock/{id}")
+    public String updateStock(@RequestParam int id, @RequestParam int quantity) {
+
+    }
