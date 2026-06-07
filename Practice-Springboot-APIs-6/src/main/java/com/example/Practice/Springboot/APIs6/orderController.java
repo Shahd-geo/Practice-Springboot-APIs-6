0a91987389f32f35309fb0aa725c78556f6eb845
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 12:06:44 +0400

    Preload the map with  one sample order

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/orderController.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/orderController.java
@@ -2,6 +2,12 @@ package com.example.Practice.Springboot.APIs6;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class orderController {
+    private HashMap<Integer, Order> orders =new  HashMap<>();
+    public orderController() {
+        orders.put(1, new Order(1, "Muscat", " Delivered"));
+    }
 }
