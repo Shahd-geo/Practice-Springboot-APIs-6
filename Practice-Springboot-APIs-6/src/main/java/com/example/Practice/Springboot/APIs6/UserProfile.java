8be85d8e36ef9b48f3a90a8aa9cec7dfd1665de2
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 11:06:34 +0400

    Create a constructor WITH OUT USING LOMBOK

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/UserProfile.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/UserProfile.java
@@ -1,4 +1,41 @@
 package com.example.Practice.Springboot.APIs6;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class UserProfile {
+    private int userId;
+    private int phoneNumber;
+    private String address;
+
+    public UserProfile(int userId, int phoneNumber, String address) {
+        this.userId = userId;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
