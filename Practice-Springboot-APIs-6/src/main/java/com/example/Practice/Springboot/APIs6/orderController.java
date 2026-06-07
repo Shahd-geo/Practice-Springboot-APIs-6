d1ec23aa23e51041eade9215c5f41fc6d209b57c
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 12:18:41 +0400

    لاقهىل خقيثق هي بقخة اشساةشح

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/orderController.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/orderController.java
@@ -11,13 +11,14 @@ import java.util.HashMap;
 public class orderController {
     private HashMap<Integer, Order> orders =new  HashMap<>();
     public orderController() {
-        orders.put(1, new Order(1, "Muscat", " Delivered"));
+        orders.put(101, new Order(101, "Muscat", "Pending"));
     }
     @PutMapping("/updateOorder/{orderId}")
     public String updateOrder(@PathVariable int orderId, @RequestParam String address,@RequestParam String status) {
         if (!orders.containsKey(orderId)) {
             return "Order not found";
         }
+        Order order = orders.get(orderId);
 
     }
 
