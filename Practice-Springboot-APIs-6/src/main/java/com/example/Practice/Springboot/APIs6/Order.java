5e62aa9a3af980d0fe0c08c252592c1f8983a9ed
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 12:02:18 +0400

    USING LOMBOK NOTATION @DATA TO CREATR  getter and setter

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/Order.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/Order.java
@@ -1,5 +1,12 @@
 package com.example.Practice.Springboot.APIs6;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Order {
     private  int orderId;
     private  String shippingAddress;
