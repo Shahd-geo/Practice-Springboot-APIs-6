c4ccef41c989972c0a4a4f7effbd9e53079d8bca
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 11:13:18 +0400

    Display the updated user profile information after modifications

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/UpdateEmailDemo.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/UpdateEmailDemo.java
@@ -3,6 +3,7 @@ package com.example.Practice.Springboot.APIs6;
 public class UpdateEmailDemo {
     public static void main(String[] args) {
         updateEmail();
+       updateUserProfile();
     }
 
     public static void updateEmail() {
@@ -40,7 +41,7 @@ public class UpdateEmailDemo {
         System.out.println("User ID: " + user.getUserId());
         System.out.println("Phone Number: " + user.getPhoneNumber());
         System.out.println("Address: " + user.getAddress());
-    // update operation by changing at least two fields using setter methods:
+    // update operation by changing phone + Address  using setter methods:
         user.setPhoneNumber(67894245);
         user.setAddress("ALSeeb, Muscat");
         System.out.println("\nAfter Update");
