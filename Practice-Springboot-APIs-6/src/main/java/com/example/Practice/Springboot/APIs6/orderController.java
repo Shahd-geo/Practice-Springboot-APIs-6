15b64027f5604dc7e54b3c35b70b7d53d7ed0704
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 12:19:24 +0400

    bring order id from hashmap

--- Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/orderController.java
+++ Practice-Springboot-APIs-6/src/main/java/com/example/Practice/Springboot/APIs6/orderController.java
@@ -9,17 +9,20 @@ import java.util.HashMap;
 
 @RestController
 public class orderController {
-    private HashMap<Integer, Order> orders =new  HashMap<>();
+    private HashMap<Integer, Order> orders = new HashMap<>();
+
     public orderController() {
         orders.put(101, new Order(101, "Muscat", "Pending"));
     }
+
     @PutMapping("/updateOorder/{orderId}")
-    public String updateOrder(@PathVariable int orderId, @RequestParam String address,@RequestParam String status) {
+    public String updateOrder(@PathVariable int orderId, @RequestParam String address, @RequestParam String status) {
         if (!orders.containsKey(orderId)) {
             return "Order not found";
         }
         Order order = orders.get(orderId);
 
     }
+}
 
 }
